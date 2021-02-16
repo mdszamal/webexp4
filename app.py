00052b487a2619949cf8afe41d7b9a7d650cc1e4
mdszamal <78942241+mdszamal@users.noreply.github.com>
2021-02-16 12:54:00 +1000

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    features = [x for x in request.form.values()]
+    final_features = [np.array(features)]
+    column_names=['R&DSpend','Administration','MarketingSpend','State']
+    final_features=pd.DataFrame(final_features,columns=column_names)
+    prediction= model.predict(final_features)
+    temp=0.0
+    for i in prediction:
+        for j in i:
+            temp=j
+    return render_template('index.html', prediction_text='${}'.format(temp))
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
